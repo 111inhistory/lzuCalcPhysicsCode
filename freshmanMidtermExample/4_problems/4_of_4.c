b5fc8b41c4683913709f5d511fe044b10e775c07
111inhistory <59101115+111inhistory@users.noreply.github.com>
2024-11-09 20:06:45 +0800

    Update 4_of_4.c

--- freshmanMidtermExample/4_problems/4_of_4.c
+++ freshmanMidtermExample/4_problems/4_of_4.c
@@ -11,6 +11,9 @@ other base vector.
 #include <stdlib.h>
 #include <string.h>
 
+/*
+return the length of the vec
+*/
 double vec_mod(double *vec, int n) {
     double res = 0;
     for (int i = 0; i < n; i++) {
@@ -20,6 +23,9 @@ double vec_mod(double *vec, int n) {
     return res;
 }
 
+/*
+return the dot multi of two vecs
+*/
 double dot_multi(double *vec1, double *vec2, int n) {
     double res = 0;
     for (int i = 0; i < n; i++) {
@@ -28,6 +34,9 @@ double dot_multi(double *vec1, double *vec2, int n) {
     return res;
 }
 
+/*
+write back the vec with each elem * scalar
+*/
 void vec_multi_scalar(double *vec, double scalar, int n) {
     for (int i = 0; i < n; i++) {
         vec[i] *= scalar;
@@ -35,25 +44,26 @@ void vec_multi_scalar(double *vec, double scalar, int n) {
 }
 
 /*
-DO NOT FORGET FREE IT!
-
 n is the length of the vector array.
 */
 void vec_add(double *vec1, double *vec2, int n) {
     for (int i = 0; i < n; i++) {
-        vec1[i] = vec1[i] + vec2[2];
+        // You never know what mistake you will make.
+        // You never know what mistake you will make.
+        // You never know what mistake you will make.
+        // You never know what mistake you will make
+        // I wrote `vec1[i] = vec1[i] + vec2[2];`
+        // OMG!
+        vec1[i] = vec1[i] + vec2[i];
     }
 }
 
 /*
-DO NOT FORGET FREE IT!
-
 n is the length of the vector array.
 */
 void vec_minus(double *vec1, double *vec2, int n) {
-    double *res = (double *)malloc(n * sizeof(double));
     for (int i = 0; i < n; i++) {
-        res[i] = vec1[i] - vec2[2];
+        vec1[i] = vec1[i] - vec2[i];
     }
 }
 
@@ -64,32 +74,40 @@ void vec_minus(double *vec1, double *vec2, int n) {
 
     `n` is the dimension(阶数) of the vecs
 */
-double **gram_schmidt_o15n(double **vec_arr, int length, int n) {
-    double **res = (double **)malloc(n * length * sizeof(double));
+void gram_schmidt_o15n(double *vec_arr, int length, int n, double *res) {
+    double *temp = (double *)malloc(n * sizeof(double));
+    // copy the vec to the res
+    memcpy(res, vec_arr, length * n * sizeof(double));
     for (int i = 0; i < length; i++) {
-        double *cur_in_vec = vec_arr[i];
-        double *cur_res_vec = res[i];
-        for (int k = 0; k < n; k++) {
-            cur_res_vec[k] = cur_in_vec[k];
-        }
-        memcpy(cur_res_vec, cur_in_vec, n * sizeof(double));
+        // two array pointer represent the array current processing
+        double *cur_in_vec = &vec_arr[i * n];
+        double *cur_res_vec = &res[i * n];
         for (int j = 0; j < i; j++) {
             // (dot_multi / pow(vecmod of the base_vec, 2)) * base vector
-            double base_vec_mod = vec_mod(res[j], n);
-            double *temp = (double *)malloc(n * sizeof(double));
-            memcpy(temp, res[j], n * sizeof(double));
+            double base_vec_mod = vec_mod(&res[j * n], n);
+            memcpy(temp, &res[j * n], n * sizeof(double));
             vec_multi_scalar(temp,
-                             dot_multi(cur_in_vec, res[j], n) / base_vec_mod /
-                                 base_vec_mod,
+                             dot_multi(cur_in_vec, &res[j * n], n) /
+                                 base_vec_mod / base_vec_mod,
                              n);
             vec_minus(cur_res_vec, temp, n);
-            free(temp);
         }
     }
-    return res;
+    free(temp);
 }
 
 int main() {
-    double test_vec1[3][3] = {{1.0, 1.0, 1.0}, {3.0, 4.0, 5.0}, {6.0, 7.0, 8.0}};
+    double test_vec1[3][3] = {
+        {1.0, 1.0, 1.0}, {3.0, 4.0, 5.0}, {6.0, 7.0, 9.0}};
+    double res[3][3];
+    memset(res, 0, 3 * 3 * sizeof(double));
     // Test vec_mod
-} 
+    gram_schmidt_o15n((double *)test_vec1, 3, 3, (double *)res);
+    for (int i = 0; i < 3; i++) {
+        printf("{");
+        for (int j = 0; j < 3; j++) {
+            printf("%lf, ", res[i][j]);
+        }
+        printf("}\n");
+    }
+}
