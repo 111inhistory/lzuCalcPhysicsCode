a0d5bd45b214c4d7772ef1e6ff2dc40ad14fc015
111inhistory <59101115+111inhistory@users.noreply.github.com>
2024-11-21 22:59:22 +0800

    feat: Make Code Greater Again!

--- examRelated/freshmanMidtermExample/realProblem/P8.c
+++ examRelated/freshmanMidtermExample/realProblem/P8.c
@@ -1,73 +1,153 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
-int linear_filter1(int *filter, int *prime, int max);
-int linear_filter2(int *filter, int *prime, int max);
+#define ll long long
+#define BIT_MASK 0x7
+#define BYTE_MASK 0xFF
+
+ll linear_filter1(ll *prime, ll max);
+ll linear_filter2(char *filter, ll *prime, ll max);
 
 /*returned array length is 3. */
-int *gedebach(int n) {
-    // init linear filter array
-    int *res = (int *)malloc(3 * sizeof(int));
-    res[0] = 0, res[1] = 0, res[2] = 0;
-    int *filter = (int *)malloc((n + 1) * sizeof(int));
+ll gedebach(ll *prime, ll cnt, ll n) {
+    FILE *f = fopen("Result.txt", "w");
+    char *string = malloc(104857600 * sizeof(char));
+    setvbuf(f, string, _IOFBF, 104857600);
     // int prime[10000000];
-    int *prime = (int *)malloc(n * sizeof(int));
-    int cnt = linear_filter2(filter, prime, n);
-    int last = cnt - 1;
-    for (int i = 0; i < cnt; i++) {
-        int p1 = prime[i];
-        int remain = n-p1;
-        int j = last;
-        int p2 = prime[j];
-        while (p2 > remain) {
-            p2 = prime[--j];
-        }
-        last = j;
-        while (j > 0) {
-            if (filter[remain - p2]) {
-                res[0] = p1;
-                res[1] = p2;
-                res[2] = n - p1 - p2;
+    // ll cnt = linear_filter1(prime, n);
+    int solution = 0;
+    ll k = 0;
+    if (n % 2 == 0) {
+        ll remain = n - 2;
+        ll i = k, j = cnt - 1;
+        while (i < j) {
+            if (prime[i] + prime[j] < remain) {
+                i++;
+            } else if (prime[i] + prime[j] > remain) {
+                j--;
+            } else {
+                // printf("Found %lld + %lld = %lld\n", prime[i], prime[j], n);
+                fprintf(f, "%lld %lld %lld\n", prime[i], prime[j], prime[k]);
+                solution++;
+                j--;
             }
-            p2 = prime[--j];
         }
-        if (res[0] != 0) break;
+    } else {
+        while (k < cnt) {
+            ll remain = n - prime[k];
+            ll i = k, j = cnt - 1;
+            while (i < j) {
+                if (prime[i] + prime[j] < remain) {
+                    i++;
+                } else if (prime[i] + prime[j] > remain) {
+                    j--;
+                } else {
+                    // printf("Found %lld + %lld = %lld\n", prime[i], prime[j],
+                    // n);
+                    fprintf(f, "%lld %lld %lld\n", prime[i], prime[j],
+                            prime[k]);
+                    solution++;
+                    j--;
+                }
+            }
+            k++;
+        }
     }
-    free(filter);
-    return res;
+    fflush(f);
+    fclose(f);
+    return solution;
 }
 
 /*PLEASE ENSURE THAT PRIME IS BIG ENOUGH*/
-int linear_filter1(int *filter, int *prime, int max) {
-    int cnt = 0;
-    for (int i = 0; i < max; i++) {
-        filter[i] = 1;
+// ll linear_filter1(ll *prime, ll max) {
+//     char *filter = (char *)malloc(max * sizeof(char));
+//     ll cnt = 0;
+//     for (ll i = 2; i < max; i++) {
+//         filter[i] = 1;
+//     }
+//     filter[0] = filter[1] = 0;
+//     prime[cnt++] = 2;
+//     for (ll i = 3; i < max + 1; i += 2) {
+//         if (filter[i] == 1) {
+//             // printf("idx = %d, idx = %d\n", (int)(i + 1) >> 3,
+//             //        (int)(i & 0b0111));
+//             prime[cnt++] = i;
+//             for (ll j = i * 3; j < max + 1; j += i * 2) {
+//                 filter[j] = 0;
+//                 // printf("idx = %d, idx = %d\n", (int)(j + 1) >> 3,
+//                 //    (int)(j & 0b0111));
+//             }
+//         }
+//     }
+//     free(filter);
+//     return cnt;
+// }
+
+ll linear_filter1(ll *prime, ll n) {
+    ll l = (n >> 4) + 1;
+    char *filter = (char *)malloc((l) * sizeof(char));
+    ll cnt = 0;
+    // optimized the even part.
+    prime[cnt++] = 2;
+    for (ll i = 0; i < l; i++) {
+        filter[i] = BYTE_MASK;
     }
-    filter[0] = filter[1] = 0;
-    for (int i = 2; i < max + 1; i++) {
-        if (filter[i] == 1) {
-            prime[cnt++] = i;
-            for (int j = i * 2; j < max + 1; j += i) {
-                filter[j] = 0;
+    memset(filter, ~0, l * sizeof(char));
+    filter[0] ^= 1;
+    // `idx` & `pos` init refer as 3
+    ll idx = 0;
+    char pos = 0;
+    for (ll _i = 0; _i < (n - 3) / 2; _i++) {
+        // `pos` is the flag pos in the char elem
+        // `idx` is the index of char array
+        pos++;
+        idx += pos >> 3;
+        pos &= BIT_MASK;
+        if (filter[idx] & (1 << pos)) { // (> 0)
+            // printf("idx = %d, idx = %d\n", (int)(i + 1) >> 3,
+            //        (int)(i >> 1) & BIT_MASK);
+
+            // ALWAYS REMEMBER 2*N SHOULD LEFT SHIFT N!!!
+            ll cur_prime = (ll)(idx << 4) | (ll)(pos << 1) | 1;
+            prime[cnt++] = cur_prime;
+            ll pos1 = (cur_prime * cur_prime) >> 1;
+            ll idx1 = pos1 >> 3;
+            pos1 = pos1 & BIT_MASK;
+            for (; idx1 < l;) {
+                filter[idx1] &= ~((1 << pos1));
+                // init `pos` from `i`. `pos` is the flag pos in filter elem
+                // it may not be fast, but straight.
+                // `pos` add twice the `i`, in this circumstances it adds `i`
+                pos1 += cur_prime;
+                // so idx only adds pos / 8
+                idx1 += pos1 >> 3;
+                pos1 = pos1 & BIT_MASK;
+                // set the bit to 0
+                // printf("idx = %d, idx = %d\n", (int)(j + 1) >> 3,
+                //    (int)(j & BIT_MASK));
             }
         }
     }
+    free(filter);
     return cnt;
 }
 
 /*PLEASE ENSURE THAT ARRAY PRIME IS BIG ENOUGH*/
-int linear_filter2(int *filter, int *prime, int max) {
-    int cnt = 0;
-    for (int i = 0; i < max; i++) {
-        filter[i] = 1;
+ll linear_filter2(char *filter, ll *prime, ll max) {
+    ll cnt = 0;
+    for (ll i = 0; i < max; i++) {
+        filter[i] = i & 1;
     }
-    filter[0] = filter[1] = 0;
-    for (int i = 2; i <= max; i++) {
+    filter[1] = 0;
+    filter[2] = 1;
+    prime[cnt++] = 2;
+    for (ll i = 3; i <= max; i++) {
         if (filter[i] == 1) {
             prime[cnt++] = i;
         }
-        for (int j = 0,idx; j < cnt && (idx = (i * prime[j])) <= max; j++) {
+        for (ll j = 0, idx; j < cnt && (idx = (i * prime[j])) <= max; j++) {
             filter[idx] = 0;
             if (i % prime[j] == 0) {
                 break;
@@ -79,13 +159,18 @@ int linear_filter2(int *filter, int *prime, int max) {
 
 int main(void) {
     int n;
-    struct timespec start, end;
+    clock_t start, end;
     scanf("%d", &n);
-    clock_gettime(CLOCK_REALTIME, &start);
-    int *res = gedebach(n);
-    clock_gettime(CLOCK_REALTIME, &end);
-    printf("%ld\n", start.tv_sec - start.tv_sec);
-    printf("%d %d %d\n", res[0], res[1], res[2]);
-    free(res);
+    ll *prime = (ll *)malloc((n / 2) * sizeof(ll));
+    start = clock();
+    ll cnt = linear_filter1(prime, n);
+    end = clock();
+    printf("Create Prime Table: %ldms\n",
+           (end - start) * 1000 / CLOCKS_PER_SEC);
+    start = clock();
+    ll solution = gedebach(prime, cnt, n);
+    end = clock();
+    printf("共有%lld方案\n", solution);
+    printf("Stat Solutions: %ldms\n", (end - start) * 1000 / CLOCKS_PER_SEC);
     return 0;
 }
